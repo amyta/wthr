0a13536fe6a71badafd3c3ddfded0e3680c640b2
aahernandez <aahernandez@smu.edu>
2015-03-01 08:35:00 -0600

    geolocation

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function($) {
+
+	var state = geoplugin_region();
+    var city = geoplugin_city();
+
 	$.ajax({
-		url : "http://api.wunderground.com/api/a954d90c3e80bff7/geolookup/conditions/q/TX/Dallas.json",
+		url : "http://api.wunderground.com/api/a954d90c3e80bff7/geolookup/conditions/q/" + state + "/" + city + ".json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
 			var location = parsed_json['location']['city'];
 			var weather = parsed_json['current_observation']['weather'];
-			alert("Current temperature in " + location + " is: " + weather);
 		}
 	});
 });
